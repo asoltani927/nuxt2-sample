451cf2170bc73397ad2c334efa3dace75b07a48d
Amin Soltani <asoltani927@gmail.com>
2022-11-02 09:37:48 +0330

    create layout and base sections  Changes to be committed: 	new file:   components/base/sections/DefaultFooter.vue 	new file:   components/base/sections/HomeHeader.vue 	new file:   layouts/default.vue 	new file:   layouts/error.vue

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="default-layout" dir="rtl">
+    <base-sections-home-header />
+    <div class="container mx-auto">
+      <!-- ... -->
+    </div>
+    <base-sections-default-footer />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
